3647a0d0693793150837f5035ce06064b1641018
Oliver Denton <ojdenton1@sheffield.ac.uk>
2021-04-26 10:50:18 +0100

    pub callback function started

--- src/pub.py
+++ src/pub.py
@@ -7,7 +7,9 @@ from std_msgs.msg import String
 class Publisher:
 
     def __init__(self):
-        self.pub = rospy.Publisher('chatter', String, queue_size=10)
+        self.startup = True
+        #change params
+        self.pub = rospy.Publisher('cmd_vel', String, queue_size=10)
         rospy.init_node('publisher_node', anonymous=True)
         self.rate = rospy.Rate(10) # hz
 
@@ -28,6 +30,30 @@ class Publisher:
             publisher_message = "rospy time is: {}".format(rospy.get_time())
             self.pub.publish(publisher_message)
             self.rate.sleep()
+
+    def callback_function(self,odom_data):
+        # obtain the orientation co-ords:
+        x = odom_data.pose.pose.orientation.x
+        y = odom_data.pose.pose.orientation.y
+        z = odom_data.pose.pose.orientation.z
+        w = odom_data.pose.pose.orientation.w
+
+        # obtain the position co-ords:
+        self.x = odom_data.pose.pose.position.x
+        self.y = odom_data.pose.pose.position.y
+
+        # convert orientation co-ords to roll, pitch yaw (theta_x, theta_y, theta_z):
+        (roll, pitch, self.yaw) = euler_from_quaternion([x, y, z, w],'sxyz')
+
+        # set the initial robot pose if this node has just been launched
+        if self.startup:
+            # don't initialise again:
+            self.startup = False
+
+            # set the robot starting position:
+            self.init_x = self.x
+            self.init_y = self.y
+            self.init_yaw = self.yaw
 
 if __name__ == '__main__':
     publisher_instance = Publisher()
